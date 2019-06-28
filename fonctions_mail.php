78fd80a6ccf0f7255814566ddd07a4f4197c5d5a
Tordjman <dylantordjman@macbook-air-de-tordjman.home>
2019-06-28 14:51:05 +0200

    nettoyage et camagru presque fini

--- fonctions_mail.php
+++ fonctions_mail.php
@@ -25,8 +25,8 @@ try {
     $mail->isSMTP();                                            // Set mailer to use SMTP
     $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'elietordjman98@gmail.com';                     // SMTP username
-    $mail->Password   = 'papamaman55';                               // SMTP password
+    $mail->Username   = 'camagru1998@gmail.com';                     // SMTP username
+    $mail->Password   = 'CamagruProject42.';                               // SMTP password
     $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
